d1b84c8461fb556e66cc04061ef0c0c61a0ef930
Yavir Mykola <kolyayavir4@gmail.com>
2023-04-10 05:57:51 +0300

    documentation for song class

--- song.py
+++ song.py
@@ -1,5 +1,19 @@
 class Song:
+    """
+    Represents a song with a title, URL, and duration.
+    """
+
     def __init__(self, title=None, url=None, duration=None):
+        """
+        Initializes a new instance of the Song class.
+
+        :param title: The title of the song.
+        :type title: str
+        :param url: The URL of the song.
+        :type url: str
+        :param duration: The duration of the song in seconds.
+        :type duration: int
+        """
         self.title = title
         self.url = url
         self.duration = duration
