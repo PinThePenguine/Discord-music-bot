3a6a2da7f385ac427152f76fa01458a381583f80
Nickolay Yavir <kolyayavir4@gmail.com>
2024-02-27 21:06:35 +0200

    test: add unit tests for youtube link validation

--- tests/test_youtube_url.py
+++ tests/test_youtube_url.py
@@ -1,4 +1,7 @@
+import pytest
+
 from downloader import Youtube_downloader
+
 
 def test_valid_youtube_url() -> None:
     youtube_video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
@@ -26,3 +29,36 @@ def test_not_valid_url() -> None:
     assert Youtube_downloader.is_youtube_url(link_without_protocol) == False
     assert Youtube_downloader.is_youtube_url(link_without_protocol_and_www) == False
 
+@pytest.mark.asyncio
+async def test_available_youtube_url() -> None:
+    youtube_video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+    youtube_video_short_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    youtube_stream_url = "https://www.youtube.com/watch?v=jfKfPfyJRdk&ab_channel=LofiGirl"
+    youtube_radio_url = "https://www.youtube.com/watch?v=eOii1YaxRK8&list=RDeOii1YaxRK8&start_radio=1&ab_channel=Cringelord"
+    youtube_playlist_url = "https://www.youtube.com/playlist?list=PLaIpgnL0MSIpQV5mBSMk73V4-1KS5IGpO"
+    youtube_music_url = "https://music.youtube.com/watch?v=dQw4w9WgXcQ"
+    youtube_short_url = "https://www.youtube.com/shorts/RMiOtRFwbAg"
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_video_url) == True
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_video_short_url) == True
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_stream_url) == True
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_radio_url) == True
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_playlist_url) == True 
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_music_url) == True
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_short_url) == True
+
+@pytest.mark.asyncio
+async def test_unavailable_youtube_url() -> None:
+    youtube_unavailable_video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcX&ab_channel=RickAstley"
+    youtube_unavailable_video_short_url = "https://www.youtube.com/watch?v=dQw4w9WgXcX"
+    youtube_unavailable_stream_url = "https://www.youtube.com/watch?v=jfKfPfyJXdk&ab_channel=LofiGirl"
+    youtube_unavailable_radio_url = "https://www.youtube.com/watch?v=eOiXXYaxRK8&list=RDeOii1YaxRK8&start_radio=1&ab_channel=Cringelord"
+    youtube_unavailable_playlist_url = "https://www.youtube.com/playlist?list=PLaIpgnL0MXXpQV5mBSMk73V4-1KS5IGpO"
+    youtube_unavailable_music_url = "https://music.youtube.com/watch?v=dQw4w9WgXcX"
+    youtube_unavailable_short_url = "https://www.youtube.com/shorts/RMiXXAFCbAg"
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_unavailable_video_url) == False
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_unavailable_video_short_url) == False
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_unavailable_stream_url) == False
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_unavailable_radio_url) == False
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_unavailable_playlist_url) == False
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_unavailable_music_url) == False
+    assert await Youtube_downloader.is_valid_youtube_url(youtube_unavailable_short_url) == False
